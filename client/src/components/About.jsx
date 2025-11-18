5507d3ff248926daefed68c9c180677316a0baed
Daniel <danieladeyd77@gmail.com>
2025-11-18 02:51:06 +0000

    Custom color implemented for both light and dark mode

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -1,21 +1,27 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const About = () => {
   return (
     <section className="py-20">
       <div className="container mx-auto px-4">
-        
         <div className="grid md:grid-cols-2 gap-12 items-center">
           <div className="animate-fade-in">
             <h2 className="text-4xl font-serif font-bold mb-6">
               Craftsmanship that Lasts Generations
             </h2>
-            <p className="text-muted mb-6">
+            <p className="text-muted-foreground mb-6">
               Since 1995, Olayinka Furniture Palace has been dedicated to creating exceptional
-              urniture pieces that stand the test of time. Our skilled artisans combine traditional
-              techniques with contemporary design to deliver furniture that tells a story.{' '}
+              furniture pieces that stand the test of time. Our skilled artisans combine traditional
+              techniques with contemporary design to deliver furniture that tells a story.
             </p>
-            <p className='text-muted mb-8'> Every piece is carefully crafted using premium materials, ensuring </p>
+            <p className="text-muted-foreground mb-8 ">
+              Every piece is carefully crafted using premium materials, ensuring durability, comfort
+              and timeless elegance for your home.
+            </p>
+            <Link>
+              <button className=''> Learn More About Us</button>
+            </Link>
           </div>
 
           <div></div>
---
+++ client/src/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: 'class', // enables dark theme toggling
+  theme: {
+    extend: {}, // empty because your colors come from @theme
+  },
+  plugins: [],
+};
