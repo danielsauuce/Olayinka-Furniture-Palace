7bbe4499e76efb36786ebe3b5cba29c525cca013
Daniel <danieladeyd77@gmail.com>
2025-11-20 20:57:41 +0000

    Backend folder restructured

--- server/server.mjs
+++ server/server.mjs
@@ -1,11 +1,12 @@
 import dotenv from 'dotenv';
 dotenv.config();
-
 import express from 'express';
+import dbConnection from './config/db.mjs';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+dbConnection();
 app.use(express.json());
 
 app.listen(PORT, () => {
