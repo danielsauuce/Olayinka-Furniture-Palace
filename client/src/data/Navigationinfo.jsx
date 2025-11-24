58449c9d5d7b4aee913395f81f16d682fd6a6ad7
Daniel <danieladeyd77@gmail.com>
2025-11-24 02:03:31 +0000

    Admin page layout in progress and navigationinfo Implemented in data folder

---
+++ client/src/data/Navigationinfo.jsx
@@ -0,0 +1,7 @@
+export const navigation = [
+  { name: 'Dashboard', href: '/admin', icon: LayoutDashboard },
+  { name: 'Products', href: '/admin/products', icon: Package },
+  { name: 'Orders', href: '/admin/orders', icon: ShoppingCart },
+  { name: 'Users', href: '/admin/users', icon: Users },
+  { name: 'Analytics', href: '/admin/analytics', icon: BarChart3 },
+];
---
+++ client/src/views/admin/AdminLayout.jsx
@@ -0,0 +1,2 @@
+import { Link, useLocation } from 'react-router-dom';
+import { LayoutDashboard, Package, ShoppingCart, Users, BarChart3, Home } from 'lucide-react';
