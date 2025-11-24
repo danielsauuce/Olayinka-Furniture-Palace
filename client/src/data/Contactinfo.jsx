1d261fb951551b66577595ae69e103b27a054d2c
Daniel <danieladeyd77@gmail.com>
2025-11-24 01:18:18 +0000

    dummy data for contactinfo added to data folder

---
+++ client/src/data/Contactinfo.jsx
@@ -0,0 +1,28 @@
+import { Mail, Phone, MapPin, Clock } from 'lucide-react';
+
+export const contactInfo = [
+  {
+    icon: Phone,
+    title: 'Phone',
+    details: '+234 (0) 123 456 7890',
+    link: 'tel:+2340123456789',
+  },
+  {
+    icon: Mail,
+    title: 'Email',
+    details: 'info@olayinkafurniture.com',
+    link: 'mailto:info@olayinkafurniture.com',
+  },
+  {
+    icon: MapPin,
+    title: 'Address',
+    details: '123 Furniture Street, Victoria Island, Lagos, Nigeria',
+    link: '#',
+  },
+  {
+    icon: Clock,
+    title: 'Business Hours',
+    details: 'Mon - Sat: 9:00 AM - 6:00 PM',
+    link: '#',
+  },
+];
