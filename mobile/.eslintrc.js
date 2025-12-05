06346c72a5e3805411de6f7e491014820e61db67
Daniel Omogbolahan Adeyemi <6adeyd77@m-b118-79160.uni.solent.ac.uk>
2025-12-05 21:30:37 +0000

    eslint configure and prettier

---
+++ mobile/.eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-native/all',
+    'plugin:@typescript-eslint/recommended',
+    'prettier', // disables ESLint rules that conflict with Prettier
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'react-native', '@typescript-eslint', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    'react/react-in-jsx-scope': 'off',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,11 +1,9 @@
-import { Text, View } from "react-native";
-import '../global.css'
+import { Text, View } from 'react-native';
+import '../global.css';
 
 export default function Index() {
   return (
-    <View className="min-h-screen mt-2"
-    
-    >
+    <View className="min-h-screen mt-2">
       <Text className="bg-red-100 p-3 border">Olayinka furniture palace </Text>
     </View>
   );
