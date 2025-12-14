cf9d238d8133e77e35f9c7115de1f2a4dfcbc992
danielsauuce <danieladeyd77@gmail.com>
2025-12-14 16:37:03 +0000

    Landing page in progress

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,1 +1,16 @@
 import '../global.css';
+import { Image } from 'expo-image';
+import { useRouter } from 'expo-router';
+import React, { useEffect } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity, Dimensions } from 'react-native';
+
+const LandingPage = () => {
+  return (
+    <View>
+      <Text>LandingPage</Text>
+      <Text>Welcome to the Mobile App!</Text>
+    </View>
+  );
+};
+
+export default LandingPage;
