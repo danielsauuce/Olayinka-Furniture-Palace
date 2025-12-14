9a23f049a06b1357a76c4b8f0d300f47a5dae17b
danielsauuce <danieladeyd77@gmail.com>
2025-12-14 17:26:48 +0000

    LandingScreen modified

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -3,14 +3,18 @@ import { Image } from 'expo-image';
 import { useRouter } from 'expo-router';
 import React, { useEffect } from 'react';
 import { View, Text, StyleSheet, TouchableOpacity, Dimensions } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+import SafeScreen from '../src/components/safeScreen';
 
-const LandingPage = () => {
+const { width, height } = Dimensions.get('window');
+
+const LandingScreen = () => {
   return (
     <View>
-      <Text>LandingPage</Text>
-      <Text>Welcome to the Jesus App!</Text>
+      <Text className="mt-3 bg-dark-primary ">LandingPage</Text>
+      <Text className="text-2xl font-bold text-center mt-60">Welcome to the Jesus App!</Text>
     </View>
   );
 };
 
-export default LandingPage;
+export default LandingScreen;
