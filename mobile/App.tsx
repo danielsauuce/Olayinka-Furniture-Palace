727f70800b4b08526f94a0a1ffbfcb7b212c77a0
Daniel <danieladeyd77@gmail.com>
2025-12-18 19:18:40 +0000

    nativewind v2 configure

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,14 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
+    <View className="flex-1 items-center justify-center bg-red-400">
       <Text className="text-3xl font-bold text-blue-600">
         Olayinka Furniture Palace
       </Text>
       <Text className="mt-4 text-gray-600">
-        React Native + Expo + TypeScript + Tailwind
+        The best furniture Designer in nigeria 
       </Text>
       <StatusBar style="auto" />
     </View>
